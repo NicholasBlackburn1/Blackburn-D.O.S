0770bde692814e0865b11cba069b1647e4e1b3ab
Your Name <you@example.com>
2024-02-14 20:56:12 -0500

    OWO

--- code/main.asm
+++ code/main.asm
@@ -41,41 +41,38 @@ entry
                 sta BGCOLOR             ; Change background color
                 lda #$0d                ; Load the color light blue
                 sta BORDERCOLOR         ; Change border color
-
-                ; Clear the screen
+                
+; Clear the screen
                 ldy #$00
-clear_loop      lda #$20
+clear_loop1     lda #$20
                 sta SCREENRAM,y
                 iny
                 cpy #$e8
-                bne clear_loop
+                bne clear_loop1
 
                 ldy #$0b                ; The string "hello world!" has 12 (= $0b) characters
 
-character_loop
 
-                lda hello,y             ; Load character number y of the string
+
+; displays the  title
+title_loop
+
+                lda title,y             ; Load character number y of the string
                 sta SCREENRAM+($40*2+3),y ; Save it at position y of the screen RAM (2nd line)
                 dey                     ; Decrement y by 1
-                bpl character_loop      ; Is y positive? Then repeat
+                bpl title_loop      ; Is y positive? Then repeat
 
                 ; Additional text strings
                 ldy #$08                ; The string "blackburn dos" has 13 characters
-title_loop
-                lda title,y             ; Load character number y of the string
-                sta SCREENRAM+($40*3+7),y ; Save it at position y of the screen RAM (3rd line)
-                dey                     ; Decrement y by 1
-                bpl title_loop          ; Is y positive? Then repeat
 
                 ldy #$0b                ; The string "by nicky blackburn" has 18 characters
+; didplays the author name 
 author_loop
                 lda author,y            ; Load character number y of the string
-                sta SCREENRAM+($40*4+3),y ; Save it at position y of the screen RAM (4th line)
+                sta SCREENRAM+($40*4+30),y ; Save it at position y of the screen RAM (4th line)
                 dey                     ; Decrement y by 1
                 bpl author_loop         ; Is y positive? Then repeat
-
+    
                 rts                     ; Exit the program
-
-hello           !scr "hello world!"     ; Our string to display
-title           !scr "blackburn dos"    ; Title string
+title           !scr "nicky's dos"    ; Title string
 author          !scr "by nicky blackburn" ; Author string
